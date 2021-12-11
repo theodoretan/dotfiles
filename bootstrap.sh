7aa89f30dadb42dad71e4b431086632f6b4ad050
Theodore Tan <theodore.tan@shopify.com>
2021-12-10 20:14:33 -0500

    test

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,10 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # spaceship prompt
-sh -c "$(git clone https://github.com/spaceship-prompt/spaceship-prompt.git \"$ZSH_CUSTOM/themes/spaceship-prompt\" --depth=1)"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-ln -fs "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
+
+# ZSH_THEME="spaceship"
 
 source $ZSH/oh-my-zsh.sh -y
 
