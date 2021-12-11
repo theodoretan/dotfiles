e4adca2d464a4c40cbedd0c40480c4f01e2b93e0
Theodore Tan <theodore.tan@shopify.com>
2021-12-10 20:18:31 -0500

    make ZSH_CUSTOM a variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+export ZSH_CUSTOM="~/.oh-my-zsh/custom"
+
 # spaceship prompt
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
